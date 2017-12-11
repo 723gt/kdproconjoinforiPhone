4c34e22887d4e5c1cfdbc1c61a03818886c6e957
723 <rb.natsumi@gmail.com>
2017-12-11 11:06:59 +0900

    Initial Commit

---
+++ proconjoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proconjoin
+//
+//  Created by 吉岡捺己 on 2017/12/11.
+//  Copyright © 2017年 吉岡捺己. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
